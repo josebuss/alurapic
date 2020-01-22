62044e232dba3818cd9a4d4e3917beb02ba3039d
José Buss <josecarlosbuss@gmail.com>
2020-01-21 22:29:01 -0300

    alura
    Mais uma aula =)

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { PhotoListComponent } from './photo-list.component';
-import { PhotosComponent } from './photos/photos.component';
-import { LoadButtonComponent } from './load-button/load-button.component';
-import { FilterByDescription } from './filter-by-description.pipe';
-import { CommonModule } from '@angular/common';
-import { PhotosModule } from '../photos.module';
-import { PhotoModule } from '../photo/photo.module';
+import { NgModule } from "@angular/core";
+import { PhotoListComponent } from "./photo-list.component";
+import { PhotosComponent } from "./photos/photos.component";
+import { LoadButtonComponent } from "./load-button/load-button.component";
+import { FilterByDescription } from "./filter-by-description.pipe";
+import { CommonModule } from "@angular/common";
+import { PhotosModule } from "../photos.module";
+import { PhotoModule } from "../photo/photo.module";
+import { CardModule } from "src/app/components/card/card.module";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
-    declarations: [
-        PhotoListComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
-    imports: [
-        CommonModule,
-        PhotoModule
-    ]
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    FilterByDescription,
+    LoadButtonComponent,
+    SearchComponent
+  ],
+  imports: [CommonModule, PhotoModule, CardModule]
 })
-export class PhotoListModule { }
+export class PhotoListModule {}
