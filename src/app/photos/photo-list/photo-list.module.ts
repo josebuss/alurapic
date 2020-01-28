dac38f2aa28dcd5d2c809cc2f88d9a47dc35760a
José Buss <josecarlosbuss@gmail.com>
2020-01-27 21:40:14 -0300

    a
    Finalizado Curso

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -4,10 +4,10 @@ import { PhotosComponent } from "./photos/photos.component";
 import { LoadButtonComponent } from "./load-button/load-button.component";
 import { FilterByDescription } from "./filter-by-description.pipe";
 import { CommonModule } from "@angular/common";
-import { PhotosModule } from "../photos.module";
 import { PhotoModule } from "../photo/photo.module";
-import { CardModule } from "src/app/components/card/card.module";
 import { SearchComponent } from "./search/search.component";
+import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/darken-on-hover.module";
+import { CardModule } from "src/app/shared/components/card/card.module";
 
 @NgModule({
   declarations: [
@@ -17,6 +17,6 @@ import { SearchComponent } from "./search/search.component";
     LoadButtonComponent,
     SearchComponent
   ],
-  imports: [CommonModule, PhotoModule, CardModule]
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule]
 })
 export class PhotoListModule {}
--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,8 +1,33 @@
-import { Directive } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Renderer,
+  Input
+} from "@angular/core";
 
 @Directive({
   selector: "[apDarkenOnHover]"
 })
 export class DarkenOnHoverDirective {
-  constructor() {}
+  constructor(private el: ElementRef, private render: Renderer) { }
+
+  @Input() brightness = '70%';
+
+  @HostListener('mouseover')
+  darkenOn() {
+    this.render.setElementStyle(
+      this.el.nativeElement,
+      'filter',
+      `brightness(${this.brightness})`
+    );
+  }
+  @HostListener('mouseleave')
+  darkenOff() {
+    this.render.setElementStyle(
+      this.el.nativeElement,
+      'filter',
+      'brightness(100%)'
+    );
+  }
 }
